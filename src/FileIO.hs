480ac379e54a653aee79b202bd713f22c1983771
Toish [Graham Pegg] <techpanda@digitalpanda.ca>
2015-05-11 14:36:47 -0400

    Now works around whitespace in pco files

--- src/FileIO.hs
+++ src/FileIO.hs
@@ -1,5 +1,6 @@
 module FileIO (loadFile) where
 import Types
+import Data.Text (unpack, pack, stripStart)
 
 isComment :: String -> Bool
 isComment ('#':_) = True
@@ -9,11 +10,18 @@ isComment _       = False
 sanitise :: [String] -> [String]
 sanitise rawData = foldl (\acc x -> if (isComment x) then (acc) else (acc++[x])) [] rawData
 
+removeTabs :: String -> String
+removeTabs = filter (/='\t')
+
+removeSpaces :: [String] -> [String]
+removeSpaces (x:xs) = (unpack $ stripStart (pack x)):(removeSpaces xs)
+removeSpaces []     = []
+
 loadFile :: String -> IO (Program)
 loadFile path = do
     rawData <- readFile path
     return Program {
-    	code = (sanitise (lines rawData)),
+    	code = removeSpaces (sanitise (lines (removeTabs rawData))),
     	pointer = 0,
     	state = Running
     }
